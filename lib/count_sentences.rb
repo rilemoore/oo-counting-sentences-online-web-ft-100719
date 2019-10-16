532717d47129016a36ee377b166b6250040afde5
Riley Moore <rilemoore@gmail.com>
2019-10-16 21:31:35 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,28 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    array = self.split(/[.?!]/)
+    count = 0
+    
+    array.each do |sentence|
+      puts sentence
+      if(sentence =="")
+        array.delete_at(count)
+      end
+      count += 1
+    end
+    array.count
   end
 end
